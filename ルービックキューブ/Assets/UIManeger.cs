4efd27984bf127bb201aa65d7146148c029070e5
Syain2018 <56578598+Syain2018@users.noreply.github.com>
2021-04-22 10:49:36 +0900

    helpボタンの追加

---
+++ ルービックキューブ/Assets/UIManeger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManeger : MonoBehaviour
+{
+    //2‚Â‚ÌPanel‚ğŠi”[‚·‚é•Ï”
+    [SerializeField] GameObject gamePanel;
+    [SerializeField] GameObject helpPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    //GamePanel‚ÅHelpBotton‚ª‰Ÿ‚³‚ê‚½‚Ìˆ—
+    public void SelectGameDescription()
+    {
+        gamePanel.SetActive(false);
+        helpPanel.SetActive(true);
+    }
+
+    //HelpPanel‚ÅReturnBotton‚ª‰Ÿ‚³‚ê‚½‚Ìˆ—
+    public void SelectHelpDescription()
+    {
+        helpPanel.SetActive(false);
+        gamePanel.SetActive(true);
+    }
+}
