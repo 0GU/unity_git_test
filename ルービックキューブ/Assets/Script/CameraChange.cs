deccfb228276b6fc6b4e98a5c0543ef00c8e5930
Syain2018 <56578598+Syain2018@users.noreply.github.com>
2021-05-19 11:15:14 +0900

    カメラの向きの微調整＆ミニカメラの追加
    その他の微調整

--- ルービックキューブ/Assets/Script/CameraChange.cs
+++ ルービックキューブ/Assets/Script/CameraChange.cs
@@ -7,13 +7,9 @@ public class CameraChange : MonoBehaviour
 
     private GameObject MainCamera;      //メインカメラ格納用
 
-    //使わないカメラはコメントアウトしてね
     private GameObject Camera2;       //カメラ2格納用 
     private GameObject Camera3;       //カメラ3格納用 
     private GameObject Camera4;       //カメラ4格納用 
-    private GameObject Camera5;       //カメラ5格納用 
-    private GameObject Camera6;       //カメラ6格納用
-
 
     //呼び出し時に実行される関数
     void Start()
@@ -21,28 +17,22 @@ public class CameraChange : MonoBehaviour
         //メインカメラを取得
         MainCamera = GameObject.Find("Main Camera");
 
-        //使わないカメラはコメントアウトしてね
         Camera2 = GameObject.Find("Camera2");
         Camera3 = GameObject.Find("Camera3");
         Camera4 = GameObject.Find("Camera4");
-        Camera5 = GameObject.Find("Camera5");
-        Camera6 = GameObject.Find("Camera6");
-
 
         //カメラ2以降を非アクティブに
         Camera2.SetActive(false);
         Camera3.SetActive(false);
         Camera4.SetActive(false);
-        Camera5.SetActive(false);
-        Camera6.SetActive(false);
+    
     }
-
 
     //単位時間ごとに実行される関数
     void Update()
     {
 
-        //キーボード上1キーが押されたら、メインカメラをアクティブに
+        //キーボード上Tキーが押されたら、メインカメラをアクティブに
         if (Input.GetKeyDown(KeyCode.T))
         {
             //メインカメラをアクティブに設定
@@ -52,13 +42,9 @@ public class CameraChange : MonoBehaviour
             Camera2.SetActive(false);
             Camera3.SetActive(false);
             Camera4.SetActive(false);
-            Camera5.SetActive(false);
-            Camera6.SetActive(false);
         }
 
-
-        //【カメラ2を使わない場合コメントアウトここから】
-        //キーボード上2キーが押されたら、カメラ2をアクティブに
+        //キーボード上Yキーが押されたら、カメラ2をアクティブに
         if (Input.GetKeyDown(KeyCode.Y))
         {
             //カメラ2をアクティブに設定
@@ -68,14 +54,9 @@ public class CameraChange : MonoBehaviour
             MainCamera.SetActive(false);
             Camera3.SetActive(false);
             Camera4.SetActive(false);
-            Camera5.SetActive(false);
-            Camera6.SetActive(false);
         }
-        //【カメラ2を使わない場合コメントアウトここまで】
-
-
-        //【カメラ3を使わない場合コメントアウトここから】
-        //キーボード上3キーが押されたら、カメラ3をアクティブに
+        
+        //キーボード上Uキーが押されたら、カメラ3をアクティブに
         if (Input.GetKeyDown(KeyCode.U))
         {
             //カメラ3をアクティブに設定
@@ -85,14 +66,9 @@ public class CameraChange : MonoBehaviour
             MainCamera.SetActive(false);
             Camera2.SetActive(false);
             Camera4.SetActive(false);
-            Camera5.SetActive(false);
-            Camera6.SetActive(false);
         }
-        //【カメラ3を使わない場合コメントアウトここまで】
-
-
-        //【カメラ4を使わない場合コメントアウトここから】
-        //キーボード上4キーが押されたら、カメラ4をアクティブに
+        
+        //キーボード上Iキーが押されたら、カメラ4をアクティブに
         if (Input.GetKeyDown(KeyCode.I))
         {
             //カメラ4をアクティブに設定
@@ -102,40 +78,6 @@ public class CameraChange : MonoBehaviour
             MainCamera.SetActive(false);
             Camera2.SetActive(false);
             Camera3.SetActive(false);
-            Camera5.SetActive(false);
-            Camera6.SetActive(false);
-        }
-        //【カメラ4を使わない場合コメントアウトここまで】
-
-
-        //【カメラ5を使わない場合コメントアウトここから】
-        //キーボード上5キーが押されたら、カメラ5をアクティブに
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            //カメラ5をアクティブに設定
-            Camera5.SetActive(true);
-
-            //他のカメラを非アクティブに
-            MainCamera.SetActive(false);
-            Camera2.SetActive(false);
-            Camera3.SetActive(false);
-            Camera4.SetActive(false);
-            Camera6.SetActive(false);
-        }
-        //【カメラ5を使わない場合コメントアウトここまで】
-
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            //カメラ6をアクティブに設定
-            Camera6.SetActive(true);
-
-            //他のカメラを非アクティブに
-            MainCamera.SetActive(false);
-            Camera2.SetActive(false);
-            Camera3.SetActive(false);
-            Camera4.SetActive(false);
-            Camera5.SetActive(false);
         }
     }
 }
